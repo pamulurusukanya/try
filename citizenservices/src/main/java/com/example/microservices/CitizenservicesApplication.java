523ca197812898316723bdcc2261a2656c4af379
Sukanya <sukanyapamuluru4545@gmail.com>
2023-03-01 17:02:21 +0530

    trying to move into new repo

---
+++ citizenservices/src/main/java/com/example/microservices/CitizenservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenservicesApplication.class, args);
+	}
+
+}
